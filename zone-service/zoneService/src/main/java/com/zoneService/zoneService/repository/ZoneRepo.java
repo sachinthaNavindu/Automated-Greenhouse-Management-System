b6af36a12f7b47f8b9ce8f49a426b696e1c9b014
Sachintha <navindus485@gmail.com>
2026-04-07 00:19:46 +0530

    gateway issue fixed

--- zone-service/zoneService/src/main/java/com/zoneService/zoneService/repository/ZoneRepo.java
+++ zone-service/zoneService/src/main/java/com/zoneService/zoneService/repository/ZoneRepo.java
@@ -1,7 +1,7 @@
-package com.zoneService.zoneService.repository;
+    package com.zoneService.zoneService.repository;
 
-import com.zoneService.zoneService.entity.Zone;
-import org.springframework.data.jpa.repository.JpaRepository;
+    import com.zoneService.zoneService.entity.Zone;
+    import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ZoneRepo extends JpaRepository<Zone, String> {
-}
+    public interface ZoneRepo extends JpaRepository<Zone, String> {
+    }
